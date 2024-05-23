1d9b66007afacc2f23b574444f49b9c9104273d8
klaus-10 <82276453+klaus-10@users.noreply.github.com>
2024-05-23 12:58:08 +0200

    feat: first implementation of filterOptionsQueryTranformer()

--- server/src/utils/db.ts
+++ server/src/utils/db.ts
@@ -8,24 +8,28 @@ export default function filterOptionsQueryTranformer(filters: FilterOptions): an
       if (value) { // Only create a clause if a value exists
         switch (key) {
           case 'city':
-            query.city = { $regex: new RegExp(value, 'i') }; // Case-insensitive search
+            query["realEstate.location.city"] = { $regex: new RegExp(value, 'i') }; // Case-insensitive search
             break;
-          case 'type':
+          case 'type': // TODO; check -> acquisto o affitto
+            query["realEstate.type"] = { $regex: new RegExp(value, 'i') }; // TODO: check condition
           case 'property':
+            query["realEstate.type"] = { $regex: new RegExp(value, 'i') }; // TODO: check condition => should return for query ad || apartament
+        case 'property-type':
+            query["realEstate.isNew"] = value; // TODO: check condition => should return for query ad || apartament
           case 'autore':
-            query[key] = value;
+            query["realEstate.autore"] = value; // TODO: implement search "private" or "agency"
             break;
           case 'price':
             if (value.min && value.max) {
-              query.price = { $gte: parseFloat(value.min), $lte: parseFloat(value.max) };
+              query["realEstate.price.value"] = { $gte: parseFloat(value.min), $lte: parseFloat(value.max) };
             }
             break;
           case 'rooms':
             if (value >= 0) {
-              query.rooms = value;
+              query["realEstate.properties.0.rooms"] = value;
             }
             break;
-          case 'date':
+          case 'date': // TODO: integrate into the db the insertion date of the announcement
             if (value.from && value.to) {
               const fromDate = new Date(value.from);
               const toDate = new Date(value.to);
@@ -35,12 +39,12 @@ export default function filterOptionsQueryTranformer(filters: FilterOptions): an
             break;
           case 'mq':
             if (value.from && value.to) {
-              query.mq = { $gte: parseInt(value.from), $lte: parseInt(value.to) };
+                query["realEstate.properties.0.surface"]  = { $gte: parseInt(value.from), $lte: parseInt(value.to) };
             }
             break;
           case 'mqPrice':
             if (value.from && value.to) {
-              query.mqPrice = { $gte: parseFloat(value.from), $lte: parseFloat(value.to) };
+                query["realEstate.price.mqPrice"]= { $gte: parseFloat(value.from), $lte: parseFloat(value.to) };
             }
             break;
           default:
